df70f6f06a9d459e4716e224a6ec80213453d8cf
manar <96601859+manarkhaledx@users.noreply.github.com>
2023-12-19 03:13:28 +0200

    x/o Game Created

---
+++ app/src/main/java/com/example/xogame/XoActivity.java
@@ -0,0 +1,98 @@
+package com.example.xogame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class XoActivity extends AppCompatActivity {
+        TextView playerOneScoreTV;
+        TextView playerTwoScoreTV;
+        ConstraintLayout mainView;
+        @Override
+        protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_xo);
+            playerOneScoreTV=findViewById(R.id.tv_playerOneScore);
+            playerTwoScoreTV=findViewById(R.id.tv_playerTwoScore);
+            mainView=findViewById(R.id.xo_layout);
+        }
+        int counter=0;
+        int player1score=0;
+        int player2score=0;
+        public void onPlayerClick(View view){
+            Button clickedButton=((Button)view);
+            if(!clickedButton.getText().toString().isEmpty())
+                return;
+            if(counter%2==0){
+                clickedButton.setText("X");
+            }else {
+                clickedButton.setText("O");
+            }
+            counter++;
+            if(checkWinner("X")){
+                initBoard();
+                player1score++;
+                playerOneScoreTV.setText(player1score+"");
+
+            }else if(checkWinner("O")){
+                initBoard();
+                player2score++;
+                playerTwoScoreTV.setText(player2score+"");
+            }else if(counter==9){
+                initBoard();
+            }
+        }
+    void initBoard() {
+
+        for (int i = 0; i < 9; i++) {
+            ((Button) mainView.findViewWithTag(i + "")).setText("");
+        }
+        counter = 0;
+    }
+
+
+    private boolean checkWinner(String playerSymbol){ //check who won
+        for(int i=0;i<9;i+=3){//rows
+            if(((Button)mainView.findViewWithTag(""+i))
+                    .getText().toString().equals(playerSymbol)
+                    && ((Button)mainView.findViewWithTag(""+(i+1)))
+                    .getText().toString().equals(playerSymbol)
+                    && ((Button)mainView.findViewWithTag(""+(i+2)))
+                    .getText().toString().equals(playerSymbol)
+            ){
+                return true;
+            }
+        }for(int i=0;i<3;i++){//columns
+            if(((Button)mainView.findViewWithTag(""+i))
+                    .getText().toString().equals(playerSymbol)
+                    && ((Button)mainView.findViewWithTag((i+3)+""))
+                    .getText().toString().equals(playerSymbol)
+                    && ((Button)mainView.findViewWithTag((i+6)+""))
+                    .getText().toString().equals(playerSymbol)
+            ){
+                return true;
+            }
+        }if(((Button)mainView.findViewWithTag("0"))//diagonal
+                .getText().toString().equals(playerSymbol)
+                && ((Button)mainView.findViewWithTag((4)+""))
+                .getText().toString().equals(playerSymbol)
+                && ((Button)mainView.findViewWithTag((8)+""))
+                .getText().toString().equals(playerSymbol)
+        ){
+            return true;
+        }if(((Button)mainView.findViewWithTag("2")) //diagonal
+                .getText().toString().equals(playerSymbol)
+                && ((Button)mainView.findViewWithTag((4)+""))
+                .getText().toString().equals(playerSymbol)
+                && ((Button)mainView.findViewWithTag((6)+""))
+                .getText().toString().equals(playerSymbol)
+        ){
+            return true;
+        }
+        return false;
+    }
+}
